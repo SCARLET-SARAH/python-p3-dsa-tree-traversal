d473cdde7944920280489e8858c2c2c5ac4ba5c7
scarlet <sarahscarlet641@gmail.com>
2024-06-10 16:59:16 +0300

    complete

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,37 @@
-class Tree:
-  def __init__(self, root = None):
-    self.root = root
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
 
-  def get_element_by_id(self, id):
-    pass
+class Tree:
+    def __init__(self, data):
+        self.root = self.create_tree(data)
+
+    def create_tree(self, data):
+        if 'tag_name' not in data:
+            return None
+
+        node = TreeNode(data)
+        if 'children' in data:
+            for child_data in data['children']:
+                child = self.create_tree(child_data)
+                node.children.append(child)
+
+        return node
+
+    def get_element_by_id(self, id):
+        return self._get_element_by_id(self.root, id)
+
+    def _get_element_by_id(self, node, id):
+        if node is None:
+            return None
+
+        if 'id' in node.data and node.data['id'] == id:
+            return node.data
+
+        for child in node.children:
+            result = self._get_element_by_id(child, id)
+            if result is not None:
+                return result
+
+        return None
